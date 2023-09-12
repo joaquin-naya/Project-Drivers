abadadb7ced4b40799e5e9de26f10e399af209b9
unknown <joaquinjavier.naya@gmail.com>
2023-09-11 21:14:13 -0300

    first commit

---
+++ server/src/models/Driver.js
@@ -0,0 +1,46 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    "Driver",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      forename: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      surname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      nationality: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      dob: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createInDb: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+    },
+    { timestamps: false, freezeTableName: true }
+  );
+};
